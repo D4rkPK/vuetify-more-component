9899bfdbc83e1ad247016037b8dc0a82376b7c1c
MarcosAlberto21 <msantos@mem.gob.gt>
2022-12-18 20:10:53 -0600

    [FIX] Correccion en portabilidad del componente

--- src/components/customTable/customTable.js
+++ src/components/customTable/customTable.js
@@ -1,26 +1,20 @@
 import autoTable from 'jspdf-autotable';
 import jsPDF from "jspdf";
-
+import * as XLSX from 'xlsx/xlsx.mjs';
 
 export default {
 
 
-  props: ['search', 'columns', 'rows', 'dialog', 'item', 'default_item', 'txtCreate', 'txtEdit', 'loadingbtn',
-    'icon_edit', 'icon_delete', 'titleTable'],
+  props: ['search', 'columns', 'rows', 'titleTable', 'size_letter_title','size_letter_data'],
 
 
   data: () => ({
     Search: "",
     Columns: [],
     Rows: [],
-    Dialog: false,
-    Create: false,
-    Item: {},
-    Default_item: {},
-    TxtCreate: "",
-    TxtEdit: "",
-    Loadingbtn: false,
     TitleTable: "",
+    Size_letter_title:10,
+    Size_letter_data:10,
   }),
 
 
@@ -28,82 +22,31 @@ export default {
     console.log("hola vista de gastos");
     this.Columns = this.columns;
     this.Rows = this.rows;
+    this.Search = this.search;
     this.TitleTable = this.titleTable;
-    this.Item = this.item;
+    this.Size_letter_title = this.size_letter_title != undefined ? this.size_letter_title : 10;
+    this.Size_letter_data = this.size_letter_data != undefined ? this.size_letter_data : 10;
+
   },
 
+  watch:{
 
-  watch: {
-    Dialog(val) {
-      val || this.closeDialog();
+    Search(){
+      console.log("escribiendo", this.Search);
+      this.$emit("find", this.Search)
     }
-  },
 
-  computed: {
-    titleDialog() {
-      return this.Create ? this.TxtCreate : this.TxtEdit;
-    },
   },
 
   methods: {
 
-    editItem(item) {
-      console.log("editItem", item);
-      // this.dialog_gastos_comprometidos= true;
-      // this.crear_gasto_comprometido = false;
-      // this.item_gasto = Object.assign({}, item)
-      // console.log(this.item_gasto);
+    exportExcel() {
+      console.log(this.Rows);
+      let data = XLSX.utils.json_to_sheet(this.Rows)
+      const workbook = XLSX.utils.book_new()
+      XLSX.utils.book_append_sheet(workbook, data, this.TitleTable)
+      XLSX.writeFile(workbook, `${this.TitleTable}.xlsx`)
     },
-
-    createItem() {
-      this.Dialog = true;
-      this.Create = true;
-      console.log(this.item_gasto);
-    },
-
-    closeDialog() {
-      console.log("closeDialog");
-      this.Create = false;
-      this.Item = Object.assign({}, this.Default_item)
-    },
-
-
-
-    //   async confirmar_crearProductoGasto(){
-    //     try {
-    //       this.loading_crear = true;
-    //       let r = await this.$store.state.services.categoriasService.insertar_gastos_comprometidos(this.item_gasto)
-    //       console.log("r",r);
-    //       this.loading_crear = false;
-    //       this.$toast.success("Dato registrado exitosamente!");
-    //       this.dialog_gastos_comprometidos = false;
-    //       this.listado_productosGasto();
-    //     } catch (error) {
-    //       console.log(error);
-    //       this.$toast.error("No es posible crear este tipo de producto")
-    //     }
-    //   },
-
-    //   async confirmar_editarProductoGasto(){
-    //     try {
-    //       this.loading_crear = true;
-    //       let r = await this.$store.state.services.categoriasService.actualizar_gastos_comprometidos(this.item_gasto)
-    //       console.log("r",r);
-    //       this.loading_crear = false;
-    //       this.$toast.success("Dato actualizado exitosamente!");
-    //       this.dialog_gastos_comprometidos = false;
-    //       this.listado_productosGasto();
-    //     } catch (error) {
-    //       console.log(error);
-    //       this.$toast.error("No es posible crear este tipo de producto")
-    //     }
-    //   },
-
-    confirm() {
-      console.log("acepted change");
-      // this.crear_gasto_comprometido ? this.confirmar_crearProductoGasto() : this.confirmar_editarProductoGasto()
-    },
-
 
     async exportPdf() {
       var doc = new jsPDF({
@@ -113,43 +56,25 @@ export default {
         //  putOnlyUsedFonts:true
       })
 
-
-      doc.setFontSize(10)
-
-      // var img = new Image()
-      // img.src = imagen;
-      // doc.addImage(img, 'png', 11, 10, 100, 18)
-
-      // doc.text(50, doc.internal.pageSize.height - 20, 'Esto no es una caratúla oficial, esto no garantiza la solicitud del pedido');
-
-      // for (const key in this.folios) {
-      // doc.text(60, 33, )
+      doc.setFontSize(this.Size_letter_title)
 
       let width = doc.internal.pageSize.getWidth()
-      doc.text(this.TitleTable, width / 2, 20, { align: 'center' })
-      // doc.text(19, 30, 'PRODUCTO: ' + this.folios[key].producto)
-      // doc.text(19, 35, 'MEDIDA: ' + this.folios[key].medida_producto)
-      // let ticketData = [];
-      // let encabezado = ["NOMBRE DEL PRODUCTO", "CÓDIGO DEL PRODUCTO", "CANTIDAD SOLICITADA",];
-      // let header = Object.keys(this.filas).filter(key => !fila.includes(key));
+      doc.text(this.TitleTable, width / 2, 20, { align: 'center' });
 
-
-      // empty array of rows to fill later a bit like Excel set up.
       const tableRows = []
-
       this.Rows.forEach(item => {
         let data_to_insert = []
         for (const property in item) {
           data_to_insert.push(item[property]);
         }
-         tableRows.push(data_to_insert)
+        tableRows.push(data_to_insert)
       })
 
+      console.log(this.columns);
       autoTable(doc, {
-
-        head: [ Object.keys(this.Item)],
-        body: tableRows, startY: 40, theme: 'grid', styles: {
-          fontSize: 9,
+        head: [this.columns],
+        body: tableRows, startY: 30, theme: 'grid', styles: {
+          fontSize: this.Size_letter_data,
           fontStyle: 'Arial',
           valign: 'top',
           overflow: 'linebreak',
@@ -166,24 +91,12 @@ export default {
         },
       })
 
-      // doc.addPage()
-      // }
+
       doc.save(`${this.TitleTable}.pdf`)
 
     },
 
-    //   async listado_productosGasto(){
-    //     try {
-    //       this.loading_listado = true;
-    //       let r = await this.$store.state.services.categoriasService.listar_gastos_comprometidos();
-    //       console.log(r);
-    //       this.filas_gastos_comprometidos = r.data.data;
-    //       this.loading_listado = false;
-    //     } catch (error) {
-    //       console.log(error);
-    //       this.$toast.error("No es posible obtener la información");
-    //     }
-    //   },
+
   }
 
 
--- src/components/customTable/customTable.vue
+++ src/components/customTable/customTable.vue
@@ -1,81 +1,27 @@
 <template>
-  <div>
-    <v-card>
-      <div class="row mt-5">
-        <div class="col-md-4 mt-5 ml-5">
-          <span style="font-size: 1.3rem; font-weight: bold">
-            {{TitleTable}}
-          </span>
-        </div>
-        <div class="col-md-5 col-sm-12">
-          <v-text-field
-            v-model="Search"
-            append-icon="mdi-magnify"
-            label="buscar"
-            single-line
-            hide-details
-          ></v-text-field>
-        </div>
-        <div class="col-md-2 mt-3">
-          <v-btn @click="createItem" color="primary">Crear producto</v-btn>
-          <v-btn class="m-2" @click="exportPdf()" fab small
-            ><v-icon>picture_as_pdf</v-icon></v-btn
-          >
-        </div>
-      </div>
-      <v-data-table :headers="Columns" :items="Rows" :search="Search">
-        <template v-slot:[`item.actions`]="{ item }">
-          <v-tooltip bottom color="fb8c00">
-            <template v-slot:activator="{ on, attrs }">
-              <v-btn
-                @click="editItem(item)"
-                v-bind="attrs"
-                v-on="on"
-                icon
-                dark
-                small
-                color="white"
-              >
-                <v-icon
-                  style="
-                    background-color: #fb8c00;
-                    border-radius: 50%;
-                    width: 35px;
-                    height: 35px;
-                  "
-                  class="text-center"
-                >
-                  {{ icon_edit }}
-                </v-icon>
-              </v-btn>
-            </template>
-            <span>Editar Producto</span>
-          </v-tooltip>
-        </template>
-      </v-data-table>
-    </v-card>
 
-    <!-- DIALOG GASTOS COMPROMETIDOS -->
-    <v-dialog v-model="Dialog">
-      <v-card>
-        <h1 class="p-1 text-center">{{ titleDialog }}</h1>
-        <v-card-actions>
-          <span style="color: gray; font-weight: bold"
-            >Esriba el nombre del producto como gasto comprometido</span
-          >
-        </v-card-actions>
-        <v-card-actions>
-          <br />
-          <v-text-field label="Descripcion"> </v-text-field>
-        </v-card-actions>
-        <v-card-actions class="d-flex justify-end">
-          <v-btn color="error" @click="Dialog = false">Cancelar</v-btn>
-          <v-btn color="primary" :loading="Loadingbtn" @click="confirm">{{
-            Create ? "Crear" : "Actualizar"
-          }}</v-btn>
-        </v-card-actions>
-      </v-card>
-    </v-dialog>
-  </div>
+
+  <v-row>
+    <v-col lg="12" md="12" sm="12">
+      <span style="font-size: 1.3rem; font-weight: bold;">
+        {{ TitleTable }}
+      </span>
+    </v-col>
+
+    <v-col style="display: inline-flex;">
+      <v-text-field v-model="Search" append-icon="mdi-magnify" label="buscar" single-line hide-details
+      style="margin-right: 50px; margin-left: 50px;"></v-text-field>
+
+
+      <v-btn class="m-2" @click="exportPdf()" fab small><v-icon>picture_as_pdf</v-icon></v-btn>
+     
+      <v-btn class="m-2" @click="exportExcel()" fab small><v-icon>table_chart</v-icon></v-btn>
+    </v-col>
+   
+
+  </v-row>
+
+
+
 </template>
   <script  src="./customTable.js"/>
